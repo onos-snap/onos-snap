9d489552d45295bcf2c9b13575d7702b16d98698
Viswanath Ksp <kspviswa@onos.lxd>
2016-11-04 11:44:27 +0000

    Migrated onos build to python script

--- parts/plugins/x_onos_buck.py
+++ parts/plugins/x_onos_buck.py
@@ -1,6 +1,15 @@
 import snapcraft
+import os
 
 class XOnosBuck(snapcraft.BasePlugin):
     def build(self):
         super().build()
-        self.run(['../../../parts/plugins/build.sh', self.installdir])
+        command = ['tools/build/onos-buck']
+        command.extend(['build', 'onos'])
+        self.run(command)
+
+        command = ['tar']
+        command.extend(['-C', self.installdir])
+        command.extend(['--strip-components', '1'])
+        command.extend(['-zxf', 'buck-out/gen/tools/package/onos-package/onos.tar.gz'])
+        self.run(command)
